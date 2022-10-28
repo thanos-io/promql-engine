1a53dd6b99d8da0502361dcd14437ad18bb098ec
Giedrius Statkevičius <giedrius.statkevicius@vinted.com>
2022-10-28 15:49:06 +0300

    engine: add repro for #99 + potential fix (#101)
    * engine: add repro for #99
    
    Signed-off-by: Giedrius Statkevičius <giedrius.statkevicius@vinted.com>
    
    * execution: add fix
    
    Signed-off-by: Giedrius Statkevičius <giedrius.statkevicius@vinted.com>
    
    * execution: make a copy of labels as well
    
    Signed-off-by: Giedrius Statkevičius <giedrius.statkevicius@vinted.com>
    
    * engine: test whether identical series are handled well in func operator
    
    Signed-off-by: Giedrius Statkevičius <giedrius.statkevicius@vinted.com>
    
    Signed-off-by: Giedrius Statkevičius <giedrius.statkevicius@vinted.com>

--- engine/engine_test.go
+++ engine/engine_test.go
@@ -97,6 +97,19 @@ func TestQueriesAgainstOldEngine(t *testing.T) {
 		step  time.Duration
 	}{
 		{
+			name: "func with scalar arg that selects storage, checks whether same series handled correctly",
+			load: `load 30s
+			    thanos_cache_redis_hits_total{name="caching-bucket",service="thanos-store"} 1+1x30`,
+			query: `clamp_min(thanos_cache_redis_hits_total, scalar(max(thanos_cache_redis_hits_total) by (service))) + clamp_min(thanos_cache_redis_hits_total, scalar(max(thanos_cache_redis_hits_total) by (service)))`,
+		},
+		{
+			name: "sum + rate divided by itself",
+			load: `load 30s
+			thanos_cache_redis_hits_total{name="caching-bucket",service="thanos-store"} 1+1x30`,
+			query: `(sum(rate(thanos_cache_redis_hits_total{name="caching-bucket"}[2m])) by (service)) /
+			(sum(rate(thanos_cache_redis_hits_total{name="caching-bucket"}[2m])) by (service))`,
+		},
+		{
 			name: "stddev_over_time",
 			load: `load 30s
 					http_requests_total{pod="nginx-1"} 1+1x15
