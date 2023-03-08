6395ac2298c2f7d2300812ec54a90287ac152d2b
Michael Hoffmann <mhoffm@posteo.de>
2023-03-08 14:02:44 +0100

    logicalplan: propagate matchers should not be applied to atan
    Signed-off-by: Michael Hoffmann <mhoffm@posteo.de>

--- logicalplan/propagate_selectors.go
+++ logicalplan/propagate_selectors.go
@@ -21,8 +21,8 @@ func (m PropagateMatchersOptimizer) Optimize(expr parser.Expr) parser.Expr {
 			return
 		}
 
-		// The optimizer cannot be applied to comparison operations.
-		if binOp.Op.IsComparisonOperator() {
+		// The optimizer cannot be applied to comparison operations or 'atan2'.
+		if binOp.Op.IsComparisonOperator() || binOp.Op.String() == "atan2" {
 			return
 		}
 
