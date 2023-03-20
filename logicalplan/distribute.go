d725a51c29c4343eae3bf2e6cac21862121428f9
Filip Petkovski <filip.petkovsky@gmail.com>
2023-03-20 08:40:39 +0100

    Always match engines without external labels

--- logicalplan/distribute.go
+++ logicalplan/distribute.go
@@ -285,6 +285,9 @@ func isDistributive(expr *parser.Expr) bool {
 // matchesExternalLabels returns false if given matchers are not matching external labels.
 // If true, matchesExternalLabels also returns Prometheus matchers without those matching external labels.
 func matchesExternalLabelSet(expr parser.Expr, externalLabelSet []labels.Labels) bool {
+	if len(externalLabelSet) == 0 {
+		return true
+	}
 	selectorSet := parser.ExtractSelectors(expr)
 	for _, selectors := range selectorSet {
 		hasMatch := false
