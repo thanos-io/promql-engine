af2bd2d46e4ab9f53b229e8cfd52e365d15b3059
Alan Protasio <alanprot@gmail.com>
2023-05-23 16:40:44 -0700

    Updating Prometheus to 905a0bd63a12fa328eac4b7f2674356c283bacac
    Signed-off-by: Alan Protasio <alanprot@gmail.com>

--- execution/scan/vector_selector.go
+++ execution/scan/vector_selector.go
@@ -190,7 +190,7 @@ func selectPoint(it *storage.MemoizedSeriesIterator, ts, lookbackDelta, offset i
 	}
 	if valueType == chunkenc.ValNone || t > refTime {
 		var ok bool
-		t, v, _, fh, ok = it.PeekPrev()
+		t, v, fh, ok = it.PeekPrev()
 		if !ok || t < refTime-lookbackDelta {
 			return 0, 0, nil, false, nil
 		}
