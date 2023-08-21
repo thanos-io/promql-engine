14f9f74ee7c308736b1c523da47821ba97110d28
Giedrius Statkevičius <giedrius.statkevicius@vinted.com>
2023-08-21 16:44:16 +0300

    logicalplan: add passthrough on labels (#303)
    * logicalplan: add passthrough on labels
    
    Add passthrough optimizations on labels. This is useful if you have
    queriers chained and with labels you only want to query one of those
    queriers. This should speed up such queries a lot because it won't be
    needed to pass samples around.
    
    Signed-off-by: Giedrius Statkevičius <giedrius.statkevicius@vinted.com>
    
    * logicalplan/passthrough: update impl
    
    Use a slice to simplify the implementation.
    
    Signed-off-by: Giedrius Statkevičius <giedrius.statkevicius@vinted.com>
    
    ---------
    
    Signed-off-by: Giedrius Statkevičius <giedrius.statkevicius@vinted.com>

--- logicalplan/passthrough_test.go
+++ logicalplan/passthrough_test.go
@@ -20,9 +20,9 @@ func TestPassthrough(t *testing.T) {
 	expr, err := parser.ParseExpr(`time()`)
 	testutil.Ok(t, err)
 
-	t.Run("optimized with one engine", func(t *testing.T) {
+	t.Run("optimized with one engine, in bounds", func(t *testing.T) {
 		engines := []api.RemoteEngine{
-			newEngineMock(math.MinInt64, math.MinInt64, []labels.Labels{labels.FromStrings("region", "east"), labels.FromStrings("region", "south")}),
+			newEngineMock(math.MinInt64, math.MaxInt64, []labels.Labels{labels.FromStrings("region", "east"), labels.FromStrings("region", "south")}),
 		}
 		optimizers := []Optimizer{PassthroughOptimizer{Endpoints: api.NewStaticEndpoints(engines)}}
 
@@ -32,7 +32,7 @@ func TestPassthrough(t *testing.T) {
 		testutil.Equals(t, "remote(time())", optimizedPlan.Expr().String())
 	})
 
-	t.Run("not optimized with one engine", func(t *testing.T) {
+	t.Run("not optimized with two engines", func(t *testing.T) {
 		engines := []api.RemoteEngine{
 			newEngineMock(math.MinInt64, math.MinInt64, []labels.Labels{labels.FromStrings("region", "east"), labels.FromStrings("region", "south")}),
 			newEngineMock(math.MinInt64, math.MinInt64, []labels.Labels{labels.FromStrings("region", "west")}),
@@ -43,6 +43,82 @@ func TestPassthrough(t *testing.T) {
 		optimizedPlan := plan.Optimize(optimizers)
 
 		testutil.Equals(t, "time()", optimizedPlan.Expr().String())
+	})
+
+	t.Run("not optimized with one out of bound engine", func(t *testing.T) {
+		engines := []api.RemoteEngine{
+			newEngineMock(math.MinInt64, math.MinInt64, []labels.Labels{labels.FromStrings("region", "east"), labels.FromStrings("region", "south")}),
+		}
+		optimizers := []Optimizer{PassthroughOptimizer{Endpoints: api.NewStaticEndpoints(engines)}}
+
+		plan := New(expr, &query.Options{Start: time.Unix(0, 0), End: time.Unix(0, 0)})
+		optimizedPlan := plan.Optimize(optimizers)
+
+		testutil.Equals(t, "time()", optimizedPlan.Expr().String())
+	})
+
+	t.Run("optimized with matching labels", func(t *testing.T) {
+		selectorExpr, err := parser.ParseExpr(`{region="east"}`)
+		testutil.Ok(t, err)
+
+		engines := []api.RemoteEngine{
+			newEngineMock(math.MinInt64, math.MaxInt64, []labels.Labels{labels.FromStrings("region", "east"), labels.FromStrings("region", "south")}),
+			newEngineMock(math.MinInt64, math.MaxInt64, []labels.Labels{labels.FromStrings("region", "west")}),
+		}
+		optimizers := []Optimizer{PassthroughOptimizer{Endpoints: api.NewStaticEndpoints(engines)}}
+
+		plan := New(selectorExpr, &query.Options{Start: time.Unix(0, 0), End: time.Unix(0, 0)})
+		optimizedPlan := plan.Optimize(optimizers)
+
+		testutil.Equals(t, `remote({region="east"})`, optimizedPlan.Expr().String())
+	})
+
+	t.Run("not optimized due to multiple engines", func(t *testing.T) {
+		selectorExpr, err := parser.ParseExpr(`{region=~"east|west"}`)
+		testutil.Ok(t, err)
+
+		engines := []api.RemoteEngine{
+			newEngineMock(math.MinInt64, math.MaxInt64, []labels.Labels{labels.FromStrings("region", "east"), labels.FromStrings("region", "south")}),
+			newEngineMock(math.MinInt64, math.MaxInt64, []labels.Labels{labels.FromStrings("region", "west")}),
+		}
+		optimizers := []Optimizer{PassthroughOptimizer{Endpoints: api.NewStaticEndpoints(engines)}}
+
+		plan := New(selectorExpr, &query.Options{Start: time.Unix(0, 0), End: time.Unix(0, 0)})
+		optimizedPlan := plan.Optimize(optimizers)
+
+		testutil.Equals(t, `{region=~"east|west"}`, optimizedPlan.Expr().String())
+	})
+
+	t.Run("optimized with matching labels on matrix selector", func(t *testing.T) {
+		selectorExpr, err := parser.ParseExpr(`{region="east"}[5m]`)
+		testutil.Ok(t, err)
+
+		engines := []api.RemoteEngine{
+			newEngineMock(math.MinInt64, math.MaxInt64, []labels.Labels{labels.FromStrings("region", "east"), labels.FromStrings("region", "south")}),
+			newEngineMock(math.MinInt64, math.MaxInt64, []labels.Labels{labels.FromStrings("region", "west")}),
+		}
+		optimizers := []Optimizer{PassthroughOptimizer{Endpoints: api.NewStaticEndpoints(engines)}}
+
+		plan := New(selectorExpr, &query.Options{Start: time.Unix(0, 0), End: time.Unix(0, 0)})
+		optimizedPlan := plan.Optimize(optimizers)
+
+		testutil.Equals(t, `remote({region="east"}[5m])`, optimizedPlan.Expr().String())
+	})
+
+	t.Run("not optimized with matching labels but not matching time", func(t *testing.T) {
+		selectorExpr, err := parser.ParseExpr(`{region="east"}`)
+		testutil.Ok(t, err)
+
+		engines := []api.RemoteEngine{
+			newEngineMock(math.MinInt64, math.MinInt64, []labels.Labels{labels.FromStrings("region", "east"), labels.FromStrings("region", "south")}),
+			newEngineMock(math.MinInt64, math.MaxInt64, []labels.Labels{labels.FromStrings("region", "west")}),
+		}
+		optimizers := []Optimizer{PassthroughOptimizer{Endpoints: api.NewStaticEndpoints(engines)}}
+
+		plan := New(selectorExpr, &query.Options{Start: time.Unix(0, 0), End: time.Unix(0, 0)})
+		optimizedPlan := plan.Optimize(optimizers)
+
+		testutil.Equals(t, `{region="east"}`, optimizedPlan.Expr().String())
 	})
 
 }
