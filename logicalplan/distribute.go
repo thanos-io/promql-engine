5d3d703b98ad7a49d948a5517032079d0028ae02
Michael Hoffmann <mhoffm@posteo.de>
2023-12-04 16:31:00 +0100

    plan, engine: fix push down of binary expressions in distributed optimizer (#343)
    * plan, engine: fix value type of logical distributed nodes
    
    Signed-off-by: Michael Hoffmann <mhoffm@posteo.de>
    
    * plan: fix distributing of constant expressions
    
    Signed-off-by: Michael Hoffmann <mhoffm@posteo.de>
    
    ---------
    
    Signed-off-by: Michael Hoffmann <mhoffm@posteo.de>

--- logicalplan/distribute.go
+++ logicalplan/distribute.go
@@ -77,6 +77,8 @@ type RemoteExecution struct {
 	Engine          api.RemoteEngine
 	Query           string
 	QueryRangeStart time.Time
+
+	valueType parser.ValueType
 }
 
 func (r RemoteExecution) String() string {
@@ -90,7 +92,7 @@ func (r RemoteExecution) Pretty(level int) string { return r.String() }
 
 func (r RemoteExecution) PositionRange() posrange.PositionRange { return posrange.PositionRange{} }
 
-func (r RemoteExecution) Type() parser.ValueType { return parser.ValueTypeMatrix }
+func (r RemoteExecution) Type() parser.ValueType { return r.valueType }
 
 func (r RemoteExecution) PromQLExpr() {}
 
@@ -107,7 +109,7 @@ func (r Deduplicate) Pretty(level int) string { return r.String() }
 
 func (r Deduplicate) PositionRange() posrange.PositionRange { return posrange.PositionRange{} }
 
-func (r Deduplicate) Type() parser.ValueType { return parser.ValueTypeMatrix }
+func (r Deduplicate) Type() parser.ValueType { return r.Expressions[0].Type() }
 
 func (r Deduplicate) PromQLExpr() {}
 
@@ -119,7 +121,7 @@ func (r Noop) Pretty(level int) string { return r.String() }
 
 func (r Noop) PositionRange() posrange.PositionRange { return posrange.PositionRange{} }
 
-func (r Noop) Type() parser.ValueType { return parser.ValueTypeMatrix }
+func (r Noop) Type() parser.ValueType { return parser.ValueTypeVector }
 
 func (r Noop) PromQLExpr() {}
 
@@ -268,6 +270,7 @@ func (m DistributedExecutionOptimizer) distributeQuery(expr *parser.Expr, engine
 			Engine:          e,
 			Query:           (*expr).String(),
 			QueryRangeStart: start,
+			valueType:       (*expr).Type(),
 		})
 	}
 
@@ -287,6 +290,7 @@ func (m DistributedExecutionOptimizer) distributeAbsent(expr parser.Expr, engine
 			Engine:          engines[i],
 			Query:           expr.String(),
 			QueryRangeStart: opts.Start,
+			valueType:       expr.Type(),
 		})
 	}
 
@@ -399,9 +403,9 @@ func isDistributive(expr *parser.Expr) bool {
 		// data set. This is why we cannot push down aggregations where
 		// the operand is a binary expression.
 		// The only exception currently is pushing down binary expressions with a constant operand.
-		lhsConstant := isNumberLiteral(aggr.LHS)
-		rhsConstant := isNumberLiteral(aggr.RHS)
-		return lhsConstant || rhsConstant
+		lhsConstant := isConstantExpr(aggr.LHS)
+		rhsConstant := isConstantExpr(aggr.RHS)
+		return (lhsConstant || rhsConstant)
 	case *parser.AggregateExpr:
 		// Certain aggregations are currently not supported.
 		if _, ok := distributiveAggregations[aggr.Op]; !ok {
@@ -455,17 +459,20 @@ func matchesExternalLabels(ms []*labels.Matcher, externalLabels labels.Labels) b
 	return true
 }
 
-func isNumberLiteral(expr parser.Expr) bool {
-	if _, ok := expr.(*parser.NumberLiteral); ok {
+func isConstantExpr(expr parser.Expr) bool {
+	// TODO: there are more possibilities for constant expressions
+	switch texpr := expr.(type) {
+	case *parser.NumberLiteral:
 		return true
-	}
-
-	stepInvariant, ok := expr.(*parser.StepInvariantExpr)
-	if !ok {
+	case *parser.StepInvariantExpr:
+		return isConstantExpr(texpr.Expr)
+	case *parser.ParenExpr:
+		return isConstantExpr(texpr.Expr)
+	case *parser.BinaryExpr:
+		return isConstantExpr(texpr.LHS) && isConstantExpr(texpr.RHS)
+	default:
 		return false
 	}
-
-	return isNumberLiteral(stepInvariant.Expr)
 }
 
 func maxTime(a, b time.Time) time.Time {
