fcd980fdd6c6fb2a6a514f7a01dcbda955023c04
Michael Hoffmann <mhoffm@posteo.de>
2023-12-26 14:04:19 +0100

    plan: use custom string render method for tests
    Signed-off-by: Michael Hoffmann <mhoffm@posteo.de>

--- logicalplan/set_batch_size_test.go
+++ logicalplan/set_batch_size_test.go
@@ -98,7 +98,7 @@ func TestSetBatchSize(t *testing.T) {
 
 			plan := New(expr, &query.Options{})
 			optimizedPlan, _ := plan.Optimize(optimizers)
-			testutil.Equals(t, tcase.expected, optimizedPlan.Expr().String())
+			testutil.Equals(t, tcase.expected, renderExprTree(optimizedPlan.Expr()))
 		})
 	}
 }
