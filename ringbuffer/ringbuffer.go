37e0752acff162848b9b81e18bea07c809af07a4
Filip Petkovski <filip.petkovsky@gmail.com>
2024-01-07 09:36:53 +0100

    Use ringbuffer for windowing functions
    The current implementation for matrix selector is based on the one from
    Prometheus and uses two buffers to keep track of samples, a buffered iterator
    and a slice of decoded samples for each series. In addition to being
    harder to reason about, it is also inefficient because we traverse both
    buffers twice for each step.
    
    This commit rewrites the way points are selected for each step by using the
    ringbuffer implemented for the subquery operator. This way we only keep a
    single buffer and we don't need to manage the complexity of rotating samples
    in multiple places.
    
    Signed-off-by: Filip Petkovski <filip.petkovsky@gmail.com>

--- ringbuffer/ringbuffer.go
+++ ringbuffer/ringbuffer.go
@@ -3,6 +3,8 @@
 
 package ringbuffer
 
+import "math"
+
 type Sample[T any] struct {
 	T int64
 	V T
@@ -23,7 +25,12 @@ func (r *RingBuffer[T]) Len() int {
 	return len(r.items)
 }
 
+// MaxT returns the maximum timestamp of the ring buffer.
+// If the ring buffer is empty, it returns math.MinInt64.
 func (r *RingBuffer[T]) MaxT() int64 {
+	if len(r.items) == 0 {
+		return math.MinInt64
+	}
 	return r.items[len(r.items)-1].T
 }
 
@@ -35,6 +42,10 @@ func (r *RingBuffer[T]) ReadIntoNext(f func(*Sample[T])) {
 		r.items = append(r.items, Sample[T]{})
 	}
 	f(&r.items[n])
+}
+
+func (r *RingBuffer[T]) ReadIntoLast(f func(*Sample[T])) {
+	f(&r.items[len(r.items)-1])
 }
 
 func (r *RingBuffer[T]) Push(t int64, v T) {
@@ -54,6 +65,26 @@ func (r *RingBuffer[T]) DropBefore(ts int64) {
 	}
 	var drop int
 	for drop = 0; drop < len(r.items) && r.items[drop].T < ts; drop++ {
+	}
+	keep := len(r.items) - drop
+
+	r.tail = resize(r.tail, drop)
+	copy(r.tail, r.items[:drop])
+	copy(r.items, r.items[drop:])
+	copy(r.items[keep:], r.tail)
+	r.items = r.items[:keep]
+}
+
+func (r *RingBuffer[T]) DropBeforeWithExtLookback(ts int64, extMint int64) {
+	if len(r.items) == 0 || r.items[len(r.items)-1].T < ts {
+		r.items = r.items[:0]
+		return
+	}
+	var drop int
+	for drop = 0; drop < len(r.items) && r.items[drop].T < ts; drop++ {
+	}
+	if drop > 0 && r.items[drop-1].T >= extMint {
+		drop--
 	}
 	keep := len(r.items) - drop
 
