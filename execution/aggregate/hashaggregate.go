00e6757a42ef4ff91c56f0380934f1692fddb032
Michael Hoffmann <mhoffm@posteo.de>
2025-01-03 17:13:51 +0000

    execute: review on execution/aggregate/hashaggregate.go
    Co-authored-by: Filip Petkovski <filip.petkovsky@gmail.com>
    Signed-off-by: Michael Hoffmann <mhoffm@posteo.de>

--- execution/aggregate/hashaggregate.go
+++ execution/aggregate/hashaggregate.go
@@ -221,8 +221,7 @@ func (a *aggregate) initializeTables(ctx context.Context) error {
 
 func (a *aggregate) initializeVectorizedTables(ctx context.Context) ([]aggregateTable, []labels.Labels, error) {
 	// perform initialization of the underlying operator even if we are aggregating the labels away
-	_, err := a.next.Series(ctx)
-	if err != nil {
+	if _, err := a.next.Series(ctx); err != nil {
 		return nil, nil, err
 	}
 	tables, err := newVectorizedTables(a.stepsBatch, a.aggregation)
