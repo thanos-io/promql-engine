9405e06c46875f889a45fa5ba48c8d3499c74386
Subhramit Basu Bhowmick <subhramit.bb@live.in>
2025-02-18 23:15:12 +0530

    execution: Add support for `double_exponential_smoothing` function (#517)
    * Reset commit - add
    
    Signed-off-by: Subhramit Basu Bhowmick <subhramit.bb@live.in>
    
    * Fix failing workflows
    
    Signed-off-by: Subhramit Basu Bhowmick <subhramit.bb@live.in>
    
    * Working implementation - use matrix selector
    
    Signed-off-by: subhramit <subhramit.bb@live.in>
    
    * Rename parameter to old one, remove comment
    
    Signed-off-by: subhramit <subhramit.bb@live.in>
    
    * Refine comment for parameter
    
    Signed-off-by: subhramit <subhramit.bb@live.in>
    
    * Apply linter suggestion
    
    Signed-off-by: Subhramit Basu Bhowmick <subhramit.bb@live.in>
    
    * Improve implementation (add in explain)
    
    Signed-off-by: subhramit <subhramit.bb@live.in>
    
    * Use two parameter operator
    
    Signed-off-by: subhramit <subhramit.bb@live.in>
    
    * Move `calcTrendValue` down
    
    Signed-off-by: subhramit <subhramit.bb@live.in>
    
    ---------
    
    Signed-off-by: Subhramit Basu Bhowmick <subhramit.bb@live.in>
    Signed-off-by: subhramit <subhramit.bb@live.in>

--- ringbuffer/functions.go
+++ ringbuffer/functions.go
@@ -26,9 +26,9 @@ type FunctionArgs struct {
 	Offset           int64
 	MetricAppearedTs *int64
 
-	// Only holt-winters uses two arguments, we fall back for that.
 	// quantile_over_time and predict_linear use one, so we only use one here.
-	ScalarPoint float64
+	ScalarPoint  float64
+	ScalarPoint2 float64 // only for double_exponential_smoothing (trend factor)
 }
 
 type FunctionCall func(f FunctionArgs) (*float64, *histogram.FloatHistogram, bool, error)
@@ -297,6 +297,33 @@ var rangeVectorFuncs = map[string]FunctionCall{
 			return nil, nil, false, nil
 		}
 		v := predictLinear(f.Samples, f.ScalarPoint, f.StepTime)
+		return &v, nil, true, nil
+	},
+	"double_exponential_smoothing": func(f FunctionArgs) (*float64, *histogram.FloatHistogram, bool, error) {
+		if len(f.Samples) < 2 {
+			if len(f.Samples) == 1 && f.Samples[0].V.H != nil {
+				warnings.AddToContext(annotations.MixedFloatsHistogramsWarning, f.ctx)
+				return nil, nil, false, nil
+			}
+			return nil, nil, false, nil
+		}
+
+		// Annotate mix of float and histogram.
+		for _, s := range f.Samples {
+			if s.V.H != nil {
+				warnings.AddToContext(annotations.MixedFloatsHistogramsWarning, f.ctx)
+				return nil, nil, false, nil
+			}
+		}
+
+		sf := f.ScalarPoint  // smoothing factor or alpha
+		tf := f.ScalarPoint2 // trend factor argument or beta
+
+		v, ok := doubleExponentialSmoothing(f.Samples, sf, tf)
+		if !ok {
+			return nil, nil, false, nil
+		}
+
 		return &v, nil, true, nil
 	},
 }
@@ -733,6 +760,65 @@ func deriv(points []Sample) float64 {
 func predictLinear(points []Sample, duration float64, stepTime int64) float64 {
 	slope, intercept := linearRegression(points, stepTime)
 	return slope*duration + intercept
+}
+
+// Based on https://github.com/prometheus/prometheus/blob/8baad1a73e471bd3cf3175a1608199e27484f179/promql/functions.go#L438
+// doubleExponentialSmoothing calculates the smoothed out value for the given series.
+// It is similar to a weighted moving average, where historical data has exponentially less influence on the current data.
+// It also accounts for trends in data. The smoothing factor (0 < sf < 1), aka "alpha", affects how historical data will affect the current data.
+// A lower smoothing factor increases the influence of historical data.
+// The trend factor (0 < tf < 1), aka "beta", affects how trends in historical data will affect the current data.
+// A higher trend factor increases the influence of trends.
+// Algorithm taken from https://en.wikipedia.org/wiki/Exponential_smoothing
+func doubleExponentialSmoothing(points []Sample, sf, tf float64) (float64, bool) {
+	// Check that the input parameters are valid
+	if sf <= 0 || sf >= 1 || tf <= 0 || tf >= 1 {
+		return 0, false
+	}
+
+	// Can't do the smoothing operation with less than two points
+	if len(points) < 2 {
+		return 0, false
+	}
+
+	// Check for histograms in the samples
+	for _, s := range points {
+		if s.V.H != nil {
+			return 0, false
+		}
+	}
+
+	var s0, s1, b float64
+	// Set initial values
+	s1 = points[0].V.F
+	b = points[1].V.F - points[0].V.F
+
+	// Run the smoothing operation
+	for i := 1; i < len(points); i++ {
+		// Scale the raw value against the smoothing factor
+		x := sf * points[i].V.F
+		// Scale the last smoothed value with the trend at this point
+		b = calcTrendValue(i-1, tf, s0, s1, b)
+		y := (1 - sf) * (s1 + b)
+		s0, s1 = s1, x+y
+	}
+
+	return s1, true
+}
+
+// calcTrendValue calculates the trend value at the given index i.
+// This is somewhat analogous to the slope of the trend at the given index.
+// The argument "tf" is the trend factor.
+// The argument "s0" is the previous smoothed value.
+// The argument "s1" is the current smoothed value.
+// The argument "b" is the previous trend value.
+func calcTrendValue(i int, tf, s0, s1, b float64) float64 {
+	if i == 0 {
+		return b
+	}
+	x := tf * (s1 - s0)
+	y := (1 - tf) * b
+	return x + y
 }
 
 func resets(points []Sample) float64 {
