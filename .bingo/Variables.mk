68d0b7c4065e5c50c8ef82e8346b0d19cfbe18df
Pedro Tanaka <pedro.tanaka@shopify.com>
2025-03-28 08:54:09 +0100

    tooling: upgrade failling to version compatible with go 1.24
    Signed-off-by: Pedro Tanaka <pedro.tanaka@shopify.com>

--- .bingo/Variables.mk
+++ .bingo/Variables.mk
@@ -29,11 +29,11 @@ $(COPYRIGHT): $(BINGO_DIR)/copyright.mod
 	@echo "(re)installing $(GOBIN)/copyright-v0.0.0-20220225185207-fe763185946b"
 	@cd $(BINGO_DIR) && GOWORK=off $(GO) build -mod=mod -modfile=copyright.mod -o=$(GOBIN)/copyright-v0.0.0-20220225185207-fe763185946b "github.com/efficientgo/tools/copyright"
 
-FAILLINT := $(GOBIN)/faillint-v1.14.0
+FAILLINT := $(GOBIN)/faillint-v1.15.0
 $(FAILLINT): $(BINGO_DIR)/faillint.mod
 	@# Install binary/ries using Go 1.14+ build command. This is using bwplotka/bingo-controlled, separate go module with pinned dependencies.
-	@echo "(re)installing $(GOBIN)/faillint-v1.14.0"
-	@cd $(BINGO_DIR) && GOWORK=off $(GO) build -mod=mod -modfile=faillint.mod -o=$(GOBIN)/faillint-v1.14.0 "github.com/fatih/faillint"
+	@echo "(re)installing $(GOBIN)/faillint-v1.15.0"
+	@cd $(BINGO_DIR) && GOWORK=off $(GO) build -mod=mod -modfile=faillint.mod -o=$(GOBIN)/faillint-v1.15.0 "github.com/fatih/faillint"
 
 GOIMPORTS := $(GOBIN)/goimports-v0.29.0
 $(GOIMPORTS): $(BINGO_DIR)/goimports.mod
