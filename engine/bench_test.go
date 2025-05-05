4ec08eefee83b2d7b16ec3775543d791e179ff74
Saumya Shah <saumyabshah90@gmail.com>
2025-05-05 16:53:47 +0530

    add limitk/limit_ratio bench-test
    Signed-off-by: Saumya Shah <saumyabshah90@gmail.com>

--- engine/bench_test.go
+++ engine/bench_test.go
@@ -162,6 +162,15 @@ func BenchmarkRangeQuery(b *testing.B) {
 			storage: sixHourDataset,
 		},
 		{
+			name:    "limitk",
+			query:   `limitk(2, http_requests_total)`,
+			storage: sixHourDataset,
+		},
+		{
+			name:  "limit_ratio",
+			query: `limit_ratio(0.2, http_requests_total)`,
+		},
+		{
 			name:    "rate",
 			query:   `rate(http_requests_total[1m])`,
 			storage: sixHourDataset,
