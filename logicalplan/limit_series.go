6ca36b27ea83789611ec5713ae97e4a46e498169
Saumya Shah <saumyabshah90@gmail.com>
2025-05-27 11:24:59 +0530

    update
    Signed-off-by: Saumya Shah <saumyabshah90@gmail.com>

--- logicalplan/limit_series.go
+++ logicalplan/limit_series.go
@@ -15,8 +15,6 @@ import (
 type LimitSeriesOptmizer struct{}
 
 func (l LimitSeriesOptmizer) Optimize(plan Node, qOpts *query.Options) (Node, annotations.Annotations) {
-	var aggrVisited bool
-	var isGrouping bool
 	TraverseBottomUp(nil, &plan, func(parent, node *Node) (stop bool) {
 		switch e := (*node).(type) {
 		case *Binary:
@@ -26,14 +24,12 @@ func (l LimitSeriesOptmizer) Optimize(plan Node, qOpts *query.Options) (Node, an
 			switch e.Op {
 			case parser.LIMITK:
 				if len(e.Grouping) != 0 {
-					isGrouping = true
+					return true
 				}
-				if !aggrVisited && !isGrouping {
-					e.Limit, _ = strconv.Atoi(e.Param.String())
-				}
+				e.Limit, _ = strconv.Atoi(e.Param.String())
 			default:
 				// if limitk has any other aggregation at its downstream operator, then limit can't be imposed, else if limitk is itself downstream, then limiting is possible
-				aggrVisited = true
+				return true
 			}
 		}
 		return false
