75d41f3c0d2163d26ed50a6b3a6d9b903ff47828
Aleksandr Krivoshchekov <SuperPaintmanDeveloper@gmail.com>
2026-01-21 05:07:31 +0000

    Add unit tests for CachedEndpoints
    Signed-off-by: Aleksandr Krivoshchekov <SuperPaintmanDeveloper@gmail.com>

---
+++ api/remote_test.go
@@ -0,0 +1,89 @@
+// Copyright (c) The Thanos Community Authors.
+// Licensed under the Apache License 2.0.
+
+package api
+
+import (
+	"math"
+	"testing"
+
+	"github.com/efficientgo/core/testutil"
+	"github.com/prometheus/prometheus/model/labels"
+)
+
+func TestCachedEndpoints(t *testing.T) {
+	engines := remoteEndpointsV2Func(func(mint, maxt int64) []RemoteEngine {
+		testutil.Equals(t, int64(10), mint)
+		testutil.Equals(t, int64(20), maxt)
+		return []RemoteEngine{newEngineMock(0, 1, nil)}
+	})
+	endpoints := NewCachedEndpoints(engines)
+
+	es := getEngines(endpoints, 10, 20)
+	testutil.Equals(t, 1, len(es))
+}
+
+func TestCachedEndpointsAllEnginesByDefault(t *testing.T) {
+	engines := remoteEndpointsV2Func(func(mint, maxt int64) []RemoteEngine {
+		testutil.Equals(t, int64(math.MinInt64), mint)
+		testutil.Equals(t, int64(math.MaxInt64), maxt)
+		return []RemoteEngine{newEngineMock(0, 1, nil)}
+	})
+	endpoints := NewCachedEndpoints(engines)
+
+	es := endpoints.Engines()
+	testutil.Equals(t, 1, len(es))
+}
+
+func TestCachedEndpointsCachesEngines(t *testing.T) {
+	var calls int
+	engines := remoteEndpointsV2Func(func(mint, maxt int64) []RemoteEngine {
+		calls++
+		return []RemoteEngine{
+			newEngineMock(100*int64(calls), 1000*int64(calls), nil),
+			newEngineMock(200*int64(calls), 2000*int64(calls), nil),
+		}
+	})
+	endpoints := NewCachedEndpoints(engines)
+
+	es1 := getEngines(endpoints, 10, 10000)
+	testutil.Equals(t, 2, len(es1))
+
+	es2 := getEngines(endpoints, 20, 20000)
+	testutil.Equals(t, 2, len(es2))
+
+	testutil.Equals(t, 1, calls)
+	testutil.Equals(t, es1, es2)
+
+	// Engines must be mutable.
+	es1[0].(*engineMock).maxT = 1337
+	testutil.Equals(t, int64(1337), es1[0].MaxT())
+	testutil.Equals(t, int64(1337), es2[0].MaxT())
+}
+
+type remoteEndpointsV2Func func(mint, maxt int64) []RemoteEngine
+
+func (f remoteEndpointsV2Func) Engines() []RemoteEngine {
+	panic("Engines not implemented")
+}
+
+func (f remoteEndpointsV2Func) EnginesV2(mint, maxt int64) []RemoteEngine {
+	return f(mint, maxt)
+}
+
+type engineMock struct {
+	RemoteEngine
+	minT               int64
+	maxT               int64
+	labelSets          []labels.Labels
+	partitionLabelSets []labels.Labels
+}
+
+func (e engineMock) MaxT() int64                         { return e.maxT }
+func (e engineMock) MinT() int64                         { return e.minT }
+func (e engineMock) LabelSets() []labels.Labels          { return e.labelSets }
+func (e engineMock) PartitionLabelSets() []labels.Labels { return e.partitionLabelSets }
+
+func newEngineMock(mint, maxt int64, labelSets []labels.Labels) *engineMock {
+	return &engineMock{minT: mint, maxT: maxt, labelSets: labelSets, partitionLabelSets: labelSets}
+}
