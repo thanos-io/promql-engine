8a8f0491e056a0e3d7f98e63e6aa07e0b89d4d41
Michael Hoffmann <mhoffmann@cloudflare.com>
2026-04-01 09:16:40 +0000

    logicalplan: fix passthrough
    If we have multiple matchres for the same engine, they would not trigger
    the passthrough, consider
    
    a{region="west"} / b{region="west"}
    
    This would lead to multiple matching regions and not trigger passthrough
    properly.
    
    Signed-off-by: Michael Hoffmann <mhoffmann@cloudflare.com>

--- logicalplan/passthrough_test.go
+++ logicalplan/passthrough_test.go
@@ -105,6 +105,38 @@ func TestPassthrough(t *testing.T) {
 		testutil.Equals(t, `remote({region="east"}[5m])`, renderExprTree(optimizedPlan.Root()))
 	})
 
+	t.Run("optimized with multiple selectors matching the same engine", func(t *testing.T) {
+		multiSelectorExpr, err := parser.ParseExpr(`rate({region="east",__name__="metric_a"}[5m]) / {region="east",__name__="metric_b"}`)
+		testutil.Ok(t, err)
+
+		engines := []api.RemoteEngine{
+			newEngineMock(math.MinInt64, math.MaxInt64, []labels.Labels{labels.FromStrings("region", "east")}),
+			newEngineMock(math.MinInt64, math.MaxInt64, []labels.Labels{labels.FromStrings("region", "west")}),
+		}
+		optimizers := []Optimizer{PassthroughOptimizer{Endpoints: api.NewStaticEndpoints(engines)}}
+
+		plan, _ := NewFromAST(multiSelectorExpr, &query.Options{Start: time.Unix(0, 0), End: time.Unix(0, 0)}, PlanOptions{})
+		optimizedPlan, _ := plan.Optimize(optimizers)
+
+		testutil.Equals(t, `remote(rate({__name__="metric_a",region="east"}[5m]) / {__name__="metric_b",region="east"})`, renderExprTree(optimizedPlan.Root()))
+	})
+
+	t.Run("not optimized with selectors matching different engines", func(t *testing.T) {
+		crossPartitionExpr, err := parser.ParseExpr(`{region="east",__name__="metric_a"} / {region="west",__name__="metric_b"}`)
+		testutil.Ok(t, err)
+
+		engines := []api.RemoteEngine{
+			newEngineMock(math.MinInt64, math.MaxInt64, []labels.Labels{labels.FromStrings("region", "east")}),
+			newEngineMock(math.MinInt64, math.MaxInt64, []labels.Labels{labels.FromStrings("region", "west")}),
+		}
+		optimizers := []Optimizer{PassthroughOptimizer{Endpoints: api.NewStaticEndpoints(engines)}}
+
+		plan, _ := NewFromAST(crossPartitionExpr, &query.Options{Start: time.Unix(0, 0), End: time.Unix(0, 0)}, PlanOptions{})
+		optimizedPlan, _ := plan.Optimize(optimizers)
+
+		testutil.Equals(t, `{__name__="metric_a",region="east"} / {__name__="metric_b",region="west"}`, renderExprTree(optimizedPlan.Root()))
+	})
+
 	t.Run("not optimized with matching labels but not matching time", func(t *testing.T) {
 		selectorExpr, err := parser.ParseExpr(`{region="east"}`)
 		testutil.Ok(t, err)
